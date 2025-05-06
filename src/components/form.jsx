6b9c8261ebd58738ea1ca63d55c3761f4308eef4
Harini <nharinirmd@gmail.com>
2025-05-06 14:17:34 +0200

    Logic updated

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,18 +3,26 @@ import { allCountries } from "country-region-data";
 
 const form = () => {
   const [name, setName] = useState("");
+  const [prevName, setPrevName] = useState("");
   const [nationality, setNationality] = useState([]);
-  const [guessed, setGuessed] = useState(false);
   const [guessedCountry, setGuessedCountry] = useState("");
   const [maxProbabilty, setMaxProbability] = useState("");
-  const [countryNameMap, setCountryNameMap] = useState({});
 
-  let countryCode = [];
+let countryCode = [];
+
   async function handleOnClick() {
     try {
-      const response = await fetch(`https://api.nationalize.io/?name=${name}`);
-      const data = await response.json();
-      setNationality(data.country);
+      if (name !== "") {
+        const response = await fetch(
+          `https://api.nationalize.io/?name=${name}`
+        );
+        const data = await response.json();
+        setNationality(data.country);
+        setPrevName(name);
+        setName("");
+      } else {
+        alert("Please provide a valid name");
+      }
     } catch (error) {
       console.error(error);
     }
@@ -24,6 +32,18 @@ const form = () => {
     searchCode();
   }, [nationality]);
 
+// To toggle class for result div
+  function changeClass() {
+    const divElement = document.getElementById("resultDiv");
+    if (divElement.classList.contains("hidden")) {
+      divElement.className = "block";
+      divElement.classList.remove("hidden");
+    } else if (divElement.classList.contains("block")) {
+      divElement.className = "hidden";
+      divElement.classList.remove("block");
+    }
+  }
+  // To retrieve country code and country with maximum probabilty
   function searchCode() {
     nationality.map((country) =>
       countryCode.push(
@@ -35,27 +55,25 @@ const form = () => {
     const countryNameIndex = countryCode.indexOf(max);
     setMaxProbability(max);
     setGuessedCountry(countryCode[countryNameIndex + 1]);
-    setGuessed(true);
+    changeClass();
   }
-
-  useEffect(() => {
-    const nameMap = allCountries.reduce((acc, country) => {
-      acc[country.countryShortCode] = country.countryName;
-      return acc;
-    }, {});
-    setCountryNameMap(nameMap);
-  }, []);
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="text-4xl">Guess your Nationality</div>
+      <div className="text-4xl my-20">Guess your Nationality</div>
 
       <div>
-        <label htmlFor="name"> Enter your Lastname</label>
+        <label htmlFor="name " className="my-10 text-lg">
+          {" "}
+          Enter your Lastname
+        </label>
       </div>
-      <div className="border-2">
+      <div className="border-2 my-10">
         <input
+          className=" p-3 outline-none text-2xl"
           type="text"
+          autoFocus
+          required
           name="name"
           id="name"
           value={name}
@@ -66,15 +84,18 @@ const form = () => {
         <button
           type="submit"
           onClick={handleOnClick}
-          className="border-2 p-2 cursor-pointer m-4"
+          className="border-2 p-2 cursor-pointer m-4 hover:bg-black hover:text-white hover:rounded-md"
         >
           {" "}
           Guess !{" "}
         </button>
       </div>
 
-      <div className="5xl">
-        {guessed ? `${guessedCountry} - ${maxProbabilty}%` : ""}
+      <div className="5xl hidden" id="resultDiv">
+        The name <span className="text-2xl"> {prevName} </span>
+        is from <span className="text-2xl"> {guessedCountry} </span>
+        with max probabilty of
+        <span className="text-2xl"> {maxProbabilty}% </span>
       </div>
     </div>
   );
